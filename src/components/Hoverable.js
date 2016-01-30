c68da08a5b6c52fa90b8e607534a4855aae45f73
kerrin <kerrinenglish@gmail.com>
2016-01-30 11:48:22 +1000

    (Hoverable) use class methods instead of closures

--- src/components/Hoverable.js
+++ src/components/Hoverable.js
@@ -1,8 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 
-export default ({ children, onHoverChange }) => (
-  <div onMouseEnter={() => onHoverChange(true)}
-       onMouseLeave={() => onHoverChange(false)}>
-    {children}
-  </div>
-);
+export default class Hoverable extends Component {
+  constructor(props) {
+    super(props);
+    this.handleMouseOver = this.handleMouseOver.bind(this);
+    this.handleMouseOut = this.handleMouseOut.bind(this);
+  }
+
+  handleMouseOver() {
+    this.props.onHoverChange(true);
+  }
+
+  handleMouseOut() {
+    this.props.onHoverChange(false);
+  }
+
+  render() {
+    return (
+      <div onMouseOver={this.handleMouseOver}
+           onMouseOut={this.handleMouseOut}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
