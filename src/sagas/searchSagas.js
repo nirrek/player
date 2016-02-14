8bf7aa4baa251e2e8bb481eabd5736915364c8b8
nirrek <hi@kerr.in>
2016-02-14 11:53:28 +1000

    (searchSaga) return 100 results

--- src/sagas/searchSagas.js
+++ src/sagas/searchSagas.js
@@ -10,7 +10,7 @@ function* search(action, getQuery) {
   try {
     const tracks = yield call(
       [soundcloud, soundcloud.get],
-      '/tracks', { q: getQuery(), limit: 5 }
+      '/tracks', { q: getQuery(), limit: 100 }
     );
     yield put({ type: SEARCH_RESPONSE_SUCCESS, tracks });
   } catch (error) {
